df822703afe210b98ddaf8814c1cf07c14ef8c56
CoralSantiago <82484335+CoralSantiago@users.noreply.github.com>
2021-04-13 21:11:48 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDdylRe-3i8NF0MXDVdJ_BO3KEGKk9ZGDA",
+    authDomain: "autentifica42cs.firebaseapp.com",
+    projectId: "autentifica42cs",
+    storageBucket: "autentifica42cs.appspot.com",
+    messagingSenderId: "586440392212",
+    appId: "1:586440392212:web:6203db132f9acb8d3ef101",
+    measurementId: "G-6B0QMFEYDX"
+  )};

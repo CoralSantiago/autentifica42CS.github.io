c1654a16754b5667c57e71e7db0f50381fdf8a0f
CoralSantiago <82484335+CoralSantiago@users.noreply.github.com>
2021-04-13 22:02:16 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,6 +4,6 @@ firebase.initializeApp({
     projectId: "autentifica42cs",
     storageBucket: "autentifica42cs.appspot.com",
     messagingSenderId: "586440392212",
-    appId: "1:586440392212:web:6203db132f9acb8d3ef101",
-    measurementId: "G-6B0QMFEYDX"
+    appId: "1:586440392212:web:e15bbc1e5c3cb7713ef101",
+    measurementId: "G-4B9P9WE0W6"
   )};
